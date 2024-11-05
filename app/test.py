72be7ec6d9b938937bd7b4b0e0d4bbd575a73228
illyaro <panvalkon@gmail.com>
2024-11-05 13:55:17 +0100

    removed unnecessary files

--- app/test.py
+++
@@ -1,9 +0,0 @@
-from database_connection import MongoDBAtlas;
-
-db = MongoDBAtlas()
-
-db.connect()
-wikis = db.get_collection("Wikis")
-
-all_wikis = wikis.find()
-print("all wikis:", list(all_wikis))
