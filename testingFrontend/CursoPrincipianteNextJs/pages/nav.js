ddb9609401ca886b20672d5583e2044f2ba2462b
Diana Delita <ddelita2@gmail.com>
2024-11-19 20:12:28 +0100

    navbar

--- testingFrontend/CursoPrincipianteNextJs/pages/nav.js
+++ testingFrontend/CursoPrincipianteNextJs/pages/nav.js
@@ -1,5 +1,7 @@
 import  Link from 'next/link';
 import { useRouter } from 'next/navigation'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function Nav() {
   const router = useRouter();
@@ -8,18 +10,45 @@ export default function Nav() {
     router.push('/otra-vista');  // Navega a la vista especificada
   };
     return (
-      <nav>
-        <ul>
-          <li><Link href="/wiki">Wiki</Link></li>
-          <li><Link href="/my-articles">My Articles</Link></li>
-
-          <li><Link href="/article">Article</Link></li>
-          <li><Link href="/my-wikis">My Wikis</Link></li>
-          <li><Link href="/profile">My Profile</Link></li>
-          <li><Link href="/search">Search Bar</Link></li>
-          <li><Link href="/comment">comment</Link></li>
-        </ul> 
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/wiki">Wiki</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/my-articles">My Articles</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/article">Article</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/my-wikis">My Wikis</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/profile">My Profile</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/search">Search Bar</a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" href="/comment">Comment</a>
+        </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
       </nav>
       );
- }
+  }
 
