bf226405ade356fee86c966dfc4170435463b96d
Rafasaeza <rafasaezarana@gmail.com>
2024-12-04 10:34:47 +0100

    script para ejecución

---
+++ app/run_app.ps1
@@ -0,0 +1,8 @@
+# Crear una nueva consola y ejecutar el front-end
+Start-Process powershell -ArgumentList "cd .\front_end; npm run dev"
+
+# Crear una nueva consola y ejecutar el backend - wiki
+Start-Process powershell -ArgumentList "cd .\back_end\wiki; uvicorn wiki_controller:api --reload --port 13000"
+
+# Crear una nueva consola y ejecutar el backend - artículos
+Start-Process powershell -ArgumentList "cd .\back_end\articles; uvicorn article_controller:router --reload --port 13001"
