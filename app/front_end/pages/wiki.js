812076f2f4427942a01c36260bb42dc1b3eb4f79
paulariossanchez <80828204+paulariossanchez@users.noreply.github.com>
2024-12-17 18:34:31 +0100

    Revert "adapted wikipage to new routing style"
    This reverts commit 7d915c29a7f9f763a678c045ed49cfc244426792.

--- app/front_end/pages/wiki.js
+++ app/front_end/pages/wiki.js
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import Wiki from "../../components/wiki";
+import Wiki from "../components/wiki";
 
 function WikiPage() {
     const router = useRouter();
